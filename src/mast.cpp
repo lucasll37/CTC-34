1b62abb05ca4fba52bad6f02912ef0280e9cd653
Lucas <lucas.silva1037@gmail.com>
2023-10-19 02:22:29 -0300

    up

--- src/mast.cpp
+++ src/mast.cpp
@@ -1,101 +1,148 @@
+#include <map>
+#include <utility>
+#include <fstream>
 #include <iostream>
-#include <unordered_map>
-#include <string>
 #include <vector>
-#include <set>
 #include <fstream>
 
-const int MAX_WORD_SIZE = 100;
+#define MAX_WORD_SIZE 100
 
-class State {
-public:
-    std::unordered_map<char, State*> transitions;
+struct STATE {
+
+    STATE() {
+        isFinal = false;
+        transactions.clear();
+    }
+
+    std::map<char, std::pair<unsigned int, STATE *>> transactions;
     bool isFinal;
-    std::set<std::string> outputs;
-
-    State() : isFinal(false) {}
-
-    State* getTransition(char c) {
-        if(transitions.find(c) != transitions.end())
-            return transitions[c];
-        return nullptr;
-    }
-
-    void setTransition(char c, State* s) {
-        transitions[c] = s;
-    }
 };
 
-class Transducer {
-private:
-    std::unordered_map<std::string, State*> statesDictionary;
-    State* tempStates[MAX_WORD_SIZE];
-    State* initialState;
+class MAST {
+    std::map<STATE *, unsigned int> states;
+    STATE *tempStates[MAX_WORD_SIZE];
+    STATE *fisrtState;
+    // unsigned int nStates = 0;
 
-    State* findMinimized(State* s) {
-        // Aqui, estamos usando uma string de exemplo para a chave. Isso precisa ser ajustado para um critério real de minimização.
-        std::string key = *s->outputs.begin();  
-        if (statesDictionary.find(key) != statesDictionary.end()) {
-            return statesDictionary[key];
-        }
-        State* newState = new State(*s);
-        statesDictionary[key] = newState;
-        return newState;
-    }
+    public:
 
-public:
-    Transducer() : initialState(new State()) {
-        for(int i = 0; i < MAX_WORD_SIZE; i++) {
-            tempStates[i] = new State();
-        }
-    }
-
-    void processWords(const std::string& filename) {
-        std::ifstream input(filename);
-        if(!input.is_open()) {
-            std::cerr << "Erro ao abrir o arquivo!" << std::endl;
-            return;
+        void setTransition(STATE *state, char c, unsigned int value, STATE *nextState) {
+            state->transactions[c] = std::make_pair(value, nextState);
         }
 
-        std::string previousWord = "";
-        std::string currentWord;
-
-        while(std::getline(input, currentWord)) {
-            // Como exemplo simples, a saída é a própria palavra.
-            std::string currentOutput = currentWord;
-
-            // TODO: Adicionar lógica de transição, saída e manipulação de estados.
-
-            previousWord = currentWord;
-        }
-        input.close();
-    }
-
-    void printTransducer(std::ostream& output) {
-        output << "digraph Transducer {\n";
-        output << "\trankdir=LR;\n";
-        output << "\tnode [shape=circle];\n";
-
-        for (auto& pair : statesDictionary) {
-            State* s = pair.second;
-
-            if (s->isFinal) {
-                output << "\t\"" << pair.first << "\" [shape=doublecircle];\n";
+        STATE *getNextStateTransition(STATE *state, char c) {
+            if(state->transactions.find(c) == state->transactions.end()) {
+                return nullptr;
             }
 
-            for (auto& trans : s->transitions) {
-                output << "\t\"" << pair.first << "\" -> \"" << trans.second->outputs << "\" "
-                       << "[label=\"" << trans.first << "/" << trans.second->outputs << "\"];\n";
-            }
+            return state->transactions[c].second;
         }
 
-        output << "}\n";
-    }
+        unsigned int getValueTransition(STATE *state, char c) {
+            if(state->transactions.find(c) == state->transactions.end()) {
+                return 0;
+            }
+
+            return state->transactions[c].first;
+        }
+
+        void setFinal(STATE *state, bool isFinal) {
+            state->isFinal = isFinal;
+        }
+
+        STATE *findMinimized(STATE *s) {
+            STATE *r = nullptr;
+            bool isEqual;
+
+            for(auto &state: states) {
+
+                if(s->isFinal != state.first->isFinal) {
+                    continue;
+                }
+
+                isEqual = true;
+
+                for(auto &transictionPair: s->transactions) {
+                    if(state.first->transactions.find(transictionPair.first) == state.first->transactions.end()) {
+                        isEqual = false;
+                        break;
+                    }
+                }
+
+                if(isEqual) {
+                    r = state.first;
+                    break;
+                }
+            }
+
+            if(r == nullptr) {
+                r = new STATE();
+                r->isFinal = s->isFinal;
+                
+                for(auto &transictionPair: s->transactions) {
+                    r->transactions[transictionPair.first] = transictionPair.second;
+                }
+
+                states[r] = states.size();
+            }
+
+            return r;
+        }
+
+        void printDigraph() {
+
+            std::ofstream digraph("test.dot");
+
+            digraph << "digraph G {\n";
+            digraph << "rankdir=LR;\n";
+            digraph << "node [shape=circle];\n";
+
+            for(auto &state : states) {
+                digraph << "\tq" << state.second << " [label=\"q" << state.second << "\"];\n";
+            }
+
+            for(auto &state : states) {
+                for(auto &transition : state.first->transactions) {
+                    digraph << "\tq" << state.second << " -> q" << states[transition.second.second] << " [label=\"" << transition.first << " / " << transition.second.first << "\"];\n";
+                }
+            }
+
+            digraph << "}\n";
+        }
+
+        void generate(std::ifstream ordenatedWords) {
+            std::string previousWord = "";
+            std::string currentWord;
+            bool fistLine = true;
+
+            for(auto &tempState : tempStates) {
+                tempState = new STATE();
+            }
+
+            while(std::getline(ordenatedWords, currentWord)) {
+                if(fistLine) {
+                    previousWord = currentWord;
+                    fistLine = false;
+                    continue;
+                }
+
+                unsigned int prefixLengthPlus1 = 0;
+
+                for(unsigned int i = 0; i < currentWord.length(); i++) {
+                    if(currentWord[i] != previousWord[i]) {
+                        prefixLengthPlus1 = i + 1;
+                        break;
+                    }
+                }
+
+                for(int i = currentWord.size(); i > prefixLengthPlus1; i--) {
+                    setTransition(tempStates[i-1], currentWord[i-1], 0, findMinimized(tempStates[i]));
+                }
+
+                previousWord = currentWord;
+            }
+
+
+        }
+
 };
-
-int main() {
-    Transducer t;
-    t.processWords("input.txt");  // Supondo que o arquivo se chame input.txt
-    t.printTransducer(std::cout);  // Imprime o transdutor no formato DOT na saída padrão.
-    return 0;
-}
--- src/poc_mast.cpp
+++ src/poc_mast.cpp
@@ -1,23 +1,17 @@
-#include "mast.h"
+#include "mast.cpp"
 
 int main() {
-    
-    std::ifstream american_english;
 
-    american_english.open("./data/american-english.txt");
-
+    std::ifstream american_english("./data/dummy.txt");
     MAST mast;
 
-    if (american_english.is_open()) {
-        mast.generate(american_english);
-    }
-
-    else {
+    if (!american_english.is_open()) {
         std::cerr << "Erro ao abrir o arquivo para leitura." << std::endl;
         return 1;
     }
 
-    american_english.close();
-    
+    mast.generate(std::move(american_english));
+    mast.printDigraph();
+
     return 0;
 }
